de831fe6c6efb08ac7a93f92afedaa53fa71db87
gortega2 <35354398+gortega2@users.noreply.github.com>
2018-09-30 01:11:25 -0600

    layout for main menu created

---
+++ app/src/main/java/com/example/gortega_feelsbook/history_activity.java
@@ -0,0 +1,13 @@
+package com.example.gortega_feelsbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class history_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_activity);
+    }
+}
